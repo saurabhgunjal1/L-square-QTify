0baef283b20c5536243fe07459d1158afae1d476
Saurabh Gunjal <gunjalsaurabh16@gmail.com>
2024-03-17 16:14:24 +0530

    navbar test3

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/navbar';
 import { StyledEngineProvider } from '@mui/material/styles';
+import Landingpage from './pages/landingpage';
 function App() {
   return (
     <>
     <StyledEngineProvider injectFirst>
-<Navbar/>
+<Landingpage/>
 </StyledEngineProvider>
 </>
   );
---
+++ qtify/src/pages/landingpage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Navbar from "../components/Navbar/navbar";
+
+const Landingpage =()=>{
+    return(
+        <Navbar/>
+    )
+}
+
+export default Landingpage
